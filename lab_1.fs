8b4f7b7188b43b928354be7461dc19e2a6589a22
Elina Akhmanova <ahmanovaelina@gmail.com>
2019-12-02 21:49:45 +0100

    Add a lab 1

---
+++ lab_1.fs
@@ -0,0 +1,8 @@
+// var 1
+// input: three float digits
+// output: sum of cubed digits from input
+let sumOfCubed x y z = 
+    x ** 3.0 + y ** 3.0 + z ** 3.0
+
+System.Console.WriteLine(sumOfCubed 1.0 2.0 3.0)
+
